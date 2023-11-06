e84a8b27e232cb6722e99eb90277891d42cfa63d
Fellipe Ribeiro Martins <121965162+Fellipedev1@users.noreply.github.com>
2023-11-06 17:30:49 -0300

    comit

--- server.js
+++ server.js
@@ -1,35 +1,63 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
+const hoek = require('hoek');
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const users = [];
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'hallowen.cluster-ceycyd1m7eei.us-east-1.rds.amazonaws.com',
+  user: 'admin', 
+  password: 'senacminas', 
+  database: 'hallowen' 
+});
 
 app.post('/users', (req, res) => {
   const { username, password, cashback, availableBalance } = req.body;
-  const newUser = { username, password, cashback, availableBalance };
-  users.push(newUser);
-  res.status(201).json(newUser);
+  pool.query(
+    'INSERT INTO users (username, password, cashback, available_balance) VALUES (?, ?, ?, ?)',
+    [username, password, cashback, availableBalance],
+    (error, results) => {
+      if (error) {
+        console.error('Erro ao inserir usuário no banco de dados:', error);
+        res.status(500).json({ message: 'Erro interno do servidor' });
+      } else {
+        res.status(201).json(results);
+      }
+    }
+  );
 });
 
 app.get('/users', (req, res) => {
-  res.json(users);
+  pool.query('SELECT * FROM users', (error, results) => {
+    if (error) {
+      console.error('Erro ao recuperar usuários do banco de dados:', error);
+      res.status(500).json({ message: 'Erro interno do servidor' });
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 app.get('/users/:username', (req, res) => {
   const { username } = req.params;
-  const user = users.find(u => u.username === username);
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: 'Usuário não encontrado' });
-  }
+  pool.query('SELECT * FROM users WHERE username = ?', [username], (error, results) => {
+    if (error) {
+      console.error('Erro ao recuperar usuário do banco de dados:', error);
+      res.status(500).json({ message: 'Erro interno do servidor' });
+    } else if (results.length > 0) {
+      res.json(results[0]);
+    } else {
+      res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+  });
 });
 
-app.listen(PORT, '172.16.31.43', () => {
-  console.log(`Servidor está rodando em http://172.16.31.43:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando na porta ${PORT}`);
 });
