3394e537db8511f960c61f1e0646d132bd17bc39
Fellipe Ribeiro Martins <121965162+Fellipedev1@users.noreply.github.com>
2023-11-07 17:44:00 -0300

    esquema banco

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('./prisma/client'); 
 
 const app = express();
 const PORT = 3000;
