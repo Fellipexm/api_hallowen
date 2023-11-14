9f951b0435ff8fb914aa6868b0b2d12ccb7a7b63
Fellipe Ribeiro Martins <121965162+Fellipedev1@users.noreply.github.com>
2023-11-14 16:28:01 -0300

    app com routes

--- server.js
+++ server.js
@@ -1,63 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { PrismaClient } = require('./prisma/client'); 
 
 const app = express();
 const PORT = 3000;
+const IP = '172.16.31.43'; // Substitua pelo IP desejado
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const prisma = new PrismaClient();
+// Placeholder for in-memory "database"
+const users = [];
 
-app.post('/users', async (req, res) => {
-  const { username, password, cashback, availableBalance } = req.body;
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        username,
-        password,
-        cashback,
-        availableBalance,
-      },
-    });
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.error('Erro ao inserir usuário no banco de dados:', error);
-    res.status(500).json({ message: 'Erro interno do servidor' });
+// Rota para obter todos os usuários
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+// Rota para criar um novo usuário
+app.post('/users', (req, res) => {
+  const { nome, email, senha } = req.body;
+
+  // Verifique se os campos obrigatórios estão presentes e não são vazios
+  if (!nome || !email || !senha) {
+    return res.status(400).json({ message: 'Campos obrigatórios ausentes ou vazios' });
+  }
+
+  const newUser = {
+    nome,
+    email,
+    senha,
+  };
+
+  // Adicione o novo usuário à "base de dados" na memória
+  users.push(newUser);
+
+  res.status(201).json(newUser);
+});
+
+// Rota para fazer login
+app.post('/login', (req, res) => {
+  const { nome, senha } = req.body;
+
+  // Verifique se os campos obrigatórios estão presentes e não são vazios
+  if (!nome || !senha) {
+    return res.status(400).json({ message: 'Campos obrigatórios ausentes ou vazios' });
+  }
+
+  // Encontre o usuário com o nome fornecido
+  const user = users.find((u) => u.nome === nome);
+
+  if (user && user.senha === senha) {
+    // Se o usuário existe e a senha está correta, retornar informações de usuário
+    const { nome, email } = user;
+    res.json({ nome, email });
+  } else {
+    res.status(401).json({ message: 'Nome de usuário ou senha incorretos' });
   }
 });
 
-app.get('/users', async (req, res) => {
-  try {
-    const users = await prisma.user.findMany();
-    res.json(users);
-  } catch (error) {
-    console.error('Erro ao recuperar usuários do banco de dados:', error);
-    res.status(500).json({ message: 'Erro interno do servidor' });
-  }
-});
-
-app.get('/users/:username', async (req, res) => {
-  const { username } = req.params;
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        username,
-      },
-    });
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: 'Usuário não encontrado' });
-    }
-  } catch (error) {
-    console.error('Erro ao recuperar usuário do banco de dados:', error);
-    res.status(500).json({ message: 'Erro interno do servidor' });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor está rodando na porta ${PORT}`);
+app.listen(PORT, IP, () => {
+  console.log(`Servidor está rodando em http://${IP}:${PORT}`);
 });
